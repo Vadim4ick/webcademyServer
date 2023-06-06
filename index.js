a8c64f9967eee69d11ef70d9fcd7fe58a645fbdf
Vadim Firulyov <firulvv@mail.ru>
2023-06-06 18:55:15 +0500

    fix

--- index.js
+++ index.js
@@ -14,29 +14,48 @@
 // app.listen(port, () => {
 //     console.log(`Example app listening on port ${port}`)
 // })
-import jsonServer from "json-server";
-import path from "path";
 
-const server = jsonServer.create();
+//////
+// import jsonServer from "json-server";
+// import path from "path";
 
-const __dirname = path.dirname(new URL(import.meta.url).pathname);
+// const server = jsonServer.create();
 
-const router = jsonServer.router(path.resolve(__dirname, "db.json"));
+// const __dirname = path.dirname(new URL(import.meta.url).pathname);
 
-server.use(jsonServer.defaults({}));
-server.use(jsonServer.bodyParser);
+// const router = jsonServer.router(path.resolve(__dirname, "db.json"));
 
-// // Нужно для небольшой задержки, чтобы запрос проходил не мгновенно, имитация реального апи
-// server.use(async (req, res, next) => {
-//   await new Promise((res) => {
-//     setTimeout(res, 800);
-//   });
-//   next();
+// server.use(jsonServer.defaults({}));
+// server.use(jsonServer.bodyParser);
+
+// // // Нужно для небольшой задержки, чтобы запрос проходил не мгновенно, имитация реального апи
+// // server.use(async (req, res, next) => {
+// //   await new Promise((res) => {
+// //     setTimeout(res, 800);
+// //   });
+// //   next();
+// // });
+
+// server.use(router);
+
+// // запуск сервера
+// server.listen(8000, () => {
+//   console.log("server is running on 8000 port");
 // });
+
+import jsonServer from "json-server";
+// Returns an Express server
+var server = jsonServer.create();
+
+// Set default middlewares (logger, static, cors and no-cache)
+server.use(jsonServer.defaults());
+
+// Add custom routes
+// server.get('/custom', function (req, res) { res.json({ msg: 'hello' }) })
+
+// Returns an Express router
+var router = jsonServer.router("db.json");
 
 server.use(router);
 
-// запуск сервера
-server.listen(8000, () => {
-  console.log("server is running on 8000 port");
-});
+server.listen(3000);
